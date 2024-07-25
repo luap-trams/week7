5cf6b4272a81f20326e40b52449727b128331ec9
luap-trams <luap.trams@gmail.com>
2024-07-25 15:42:42 +0100

    style: DOM manipulation classwork

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -1,14 +1,14 @@
 // Applying background color to the page
-document.body.style.background = 'yellow'
+document.body.style.background = 'lime'
 // Adding a text to the id:mad
 const mad = document.getElementById('mad')
-mad.textContent = 'This is Mad oooh'
+mad.textContent = 'Samuel'
 // Selecting the first child element of ol and giving it a text
 const qsel = document.querySelector('ol>:nth-of-type(1)')
-qsel.textContent = 'getting it small small'
+qsel.textContent = 'Genesis'
 
 const qsel9 = document.querySelector('ol>:nth-of-type(9)')
-qsel9.textContent = 'it looks like im getting it'
+qsel9.textContent = 'Clara'
 // In reference to the parent ol, target the 10 child element and give it a background color of white
 const qselA10 = document.querySelectorAll('ol li')
 qselA10[9].style.backgroundColor = 'white'
@@ -17,7 +17,8 @@ const orangeCls = document.getElementsByClassName('orange')
 orangeCls[2].style.backgroundColor = 'grey'
 orangeCls[3].style.backgroundColor = 'black'
 const tag = document.getElementsByTagName('li')
-tag[3].textContent = 'This is really working'
+tag[3].textContent = 'Kamso'
+// Styling gradient for the 9th element of ol
 const bg_gradient = document.querySelectorAll('ol li')
 bg_gradient[9].style.backgroundImage = 'linear-gradient(90deg, green 40%, white 41%, white 65%, Green 66%)'
 
