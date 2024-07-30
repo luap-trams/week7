826833c90290152a70c2b21815a00d1bf77c5c7a
luap-trams <luap.trams@gmail.com>
2024-07-30 07:39:18 +0100

    feat: practice exercises

---
+++ 30DaysOfJS /datatypes.js
@@ -0,0 +1,20 @@
+let firstName = 'Paul'
+let lastName = 'Smart'
+let age = 35
+let school = Univelcity
+firstName = 'Paul'
+console.log(firstName)
+let fullNames = firstName + lastName
+console.log(fullNames)
+
+
+
+
+
+
+
+
+
+
+
+
